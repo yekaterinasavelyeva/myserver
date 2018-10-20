5969b0aacc242bf7dbabbcf1cc9df570024adfa2
Jekaterina Saveljeva <katya-a@inbox.lv>
2018-10-20 11:41:31 +0300

    create basic classes, add dependencies

---
+++ src/main/java/go/supers/server/Frontend.java
@@ -0,0 +1,25 @@
+package go.supers.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Yekaterina Savelyeva
+ * on 19.10.2018
+ */
+
+public class Frontend extends HttpServlet {
+
+    private String login = "";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/go/supers/server/Main.java
@@ -0,0 +1,25 @@
+package go.supers.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Yekaterina Savelyeva
+ * on 19.10.2018
+ */
+
+public class Main {
+
+    public static void main (String [] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server  = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+    }
+}
